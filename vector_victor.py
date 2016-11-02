085124d9286bd7f4fc2d041606e6eef2ffdf7330
Z-Ferguson <zfergusonn@gmail.com>
2016-11-02 14:36:27 -0400

    updated base files and removed linear_algebra.py and linear_algebra_tests.py

--- vector_victor.py
+++ vector_victor.py
@@ -1,2 +1,28 @@
 class ShapeError(Exception):
     pass
+
+class LinearAlgebra:
+
+# vector shape in shape()
+def shape():
+
+# vector addition in vector_add()
+def vector_add():
+
+# vector subtraction in vector_sub()
+def vector_sub():
+
+# vector sum in vector_sum()
+def vector_sum():
+
+# dot product in dot()
+def dot():
+
+# vector multiplication by a scalar in vector_multiply()
+def vector_multiply():
+
+# mean of multiple vectors in vector_mean()
+def vector_mean():
+
+# magnitude in magnitude()
+def magnitude():
