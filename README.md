f58d9bf571ec0aad899b853ff1711110b7471db0
Z-Ferguson <zfergusonn@gmail.com>
2016-11-26 22:40:17 -0500

    updated README, cleaned up repository, passed all vector tests

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # vector-victor
+
+## Files
+
+1. textminer
+⋅⋅1. vector_victor.py
+
+2. tests
+..1. vector_victor_test.py
+
+
+The purpose of this program is to work on Test Driven Development as well as work on list comprehension
+
+The program vector_victor_test.py is meant to input different numbers, vectors, and matrices to make sure the list comprehension for each comparable is written correctly and throws the correct exceptions when expected.
+
+to run, install nosetests and input "python nosetests vector_victor_test.py"
--- vector_victor.py
+++ vector_victor.py
@@ -3,15 +3,15 @@ import math
 class ShapeError(Exception):
     pass
 
-# Variables for matrices generally are in uppercase.
-# The shape of a matrix is its number of rows and columns, always in that order.
-# The number of rows is usually called n and the number of columns called k.
+
 def shape(vector):
     row = len(vector)
-    tuple = (row, )
-    return tuple
+    t = (row, )
+    return t
 
 # vector addition in vector_add()
+
+
 def vector_add(vector1, vector2):
     if shape(vector1) == shape(vector2):
         return [v1 + v2 for v1, v2 in zip(vector1, vector2)]
@@ -19,13 +19,15 @@ def vector_add(vector1, vector2):
         raise ShapeError
 
 # vector subtraction in vector_sub()
+
+
 def vector_sub(vector1, vector2):
     if shape(vector1) == shape(vector2):
         return [v1 - v2 for v1, v2 in zip(vector1, vector2)]
     else:
         raise ShapeError
 
-# vector sum in vector_sum()
+
 def vector_sum(*args):
     vector_shape = [shape(vector) for vector in args]
     if min(vector_shape) == max(vector_shape):
@@ -34,6 +36,8 @@ def vector_sum(*args):
         raise ShapeError
 
 # dot product in dot()
+
+
 def dot(vector_one, vector_two):
     if shape(vector_one) == shape(vector_two):
         return sum([v1 * v2 for v1, v2 in zip(vector_one, vector_two)])
@@ -41,13 +45,19 @@ def dot(vector_one, vector_two):
         raise ShapeError
 
 # vector multiplication by a scalar in vector_multiply()
+
+
 def vector_multiply(vector, scalar):
     return [(number * scalar) for number in vector]
 
 # mean of multiple vectors in vector_mean()
+
+
 def vector_mean(*args):
     return [(sum(x)/len(args)) for x in zip(*args)]
 
 # magnitude in magnitude()
+
+
 def magnitude(vector):
     return ((sum([x**2 for x in vector])))**(1/2)
